a921b2b48fffbfef7c04f228905a750dc5234aae
whitexudan15 <tresortakoudawhitexudan15@gmail.com>
2024-11-12 11:10:59 +0000

    environement de developpement prêt

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__) # Création de l'application flask
 
-@app.route('/')
+@app.route('/') # Definit  le chemin de la page principal (index.html)
 def home():
     return render_template('index.html'); # Renvoie la page index.html
 
